4ea2ab132146f31bc3e6df00c979bf8dff52b9dc
Lavryk Mykola <81701123+LeVasTiaN@users.noreply.github.com>
2024-12-31 01:48:45 +0100

    started task 1

---
+++ task1/1.js
@@ -0,0 +1,7 @@
+// synchronous part to check whether the async part will work correctly
+
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.find((e) => e > 2);
+console.log(result);
+
+//actual task
