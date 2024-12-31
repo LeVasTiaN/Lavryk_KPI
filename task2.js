a80dde0eb022f28550a9345a48941bf11f9cfab1
Lavryk Mykola <81701123+LeVasTiaN@users.noreply.github.com>
2024-12-31 11:24:35 +0100

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,9 +1,5 @@
 async function asyncFind(array, checkCondition, delay) {
-    let index = 0;
-    while (index < array.length) {
-        const element = array[index];
-        index++;
-
+    const promises = array.map(async (element) => {
         const startTime = Date.now();
 
         try {
@@ -19,8 +15,12 @@ async function asyncFind(array, checkCondition, delay) {
         } catch (err) {
             throw err;
         }
-    }
-    return undefined;
+        return undefined;
+    });
+
+    const results = await Promise.all(promises);
+
+    return results.find(result => result !== undefined) || undefined;
 }
 
 function asyncFindCheck(num, condition) {
@@ -34,3 +34,20 @@ function asyncFindCheck(num, condition) {
         }, 1000);
     });
 }
+
+async function demoCases() {
+    try {
+        const result1 = await asyncFind([10, 15, 20, 25, 30], (num) => asyncFindCheck(num, (num) => num % 5 === 0), 500);
+        console.log(result1);
+
+        const result2 = await asyncFind([3, 7, 11, 19], (num) => asyncFindCheck(num, (num) => num > 10), 1000);
+        console.log(result2);
+
+        const result3 = await asyncFind([1, 8, 16, 32, 64], (num) => asyncFindCheck(num, (num) => Math.log2(num) % 1 === 0), 2000);
+        console.log(result3);
+    } catch (err) {
+        console.error("Error occurred:", err);
+    }
+}
+
+demoCases();
