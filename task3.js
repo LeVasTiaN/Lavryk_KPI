e81806eced5aac74a21d4d48e2fa22eb5fa53684
Lavryk Mykola <81701123+LeVasTiaN@users.noreply.github.com>
2024-12-31 11:37:51 +0100

    Update task3.js

--- task3.js
+++ task3.js
@@ -26,3 +26,47 @@ async function asyncFind(array, checkCondition, delay, signal) {
 
     return results.find(result => result !== undefined) || undefined;
 }
+
+function asyncFindCheck(num, condition, signal) {
+    return new Promise((resolve, reject) => {
+        if (signal.aborted) {
+            return reject(new Error("Operation aborted"));
+        }
+
+        const timeout = setTimeout(() => {
+            try {
+                resolve(condition(num));
+            } catch (err) {
+                reject(err);
+            }
+        }, 1000);
+
+        signal.addEventListener('abort', () => {
+            clearTimeout(timeout);
+            reject(new Error("Operation aborted"));
+        });
+    });
+}
+
+async function demoCases() {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    try {
+        const result1 = await asyncFind([10, 15, 20, 25, 30], (num) => asyncFindCheck(num, (num) => num % 5 === 0, signal), 500, signal);
+        console.log(result1);
+
+        controller.abort();
+
+        const result2 = await asyncFind([3, 7, 11, 19], (num) => asyncFindCheck(num, (num) => num > 10, signal), 1000, signal);
+        console.log(result2);
+    } catch (err) {
+        if (err.message === "Operation aborted") {
+            console.log("The operation was canceled.");
+        } else {
+            console.error("Error occurred:", err);
+        }
+    }
+}
+
+demoCases();
